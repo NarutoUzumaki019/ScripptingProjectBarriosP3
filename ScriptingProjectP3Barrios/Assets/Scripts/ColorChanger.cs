4807c8746f80216798449473d0285afab1f6b671
NarutoUzumaki019 <145381119+NarutoUzumaki019@users.noreply.github.com>
2023-10-27 11:40:37 -0700

    I did stuff
    I did stuff

--- ScriptingProjectP3Barrios/Assets/Scripts/ColorChanger.cs
+++ ScriptingProjectP3Barrios/Assets/Scripts/ColorChanger.cs
@@ -48,16 +48,3 @@ public class VariablesAndFunctions : MonoBehaviour
         return result;
     }
 }
-public class UpdateAndFixedUpdate : MonoBehaviour
-{
-    void FixedUpdate()
-    {
-        Debug.Log("FixedUpdate time :" + Time.deltaTime);
-    }
-
-
-    void Update()
-    {
-        Debug.Log("Update time :" + Time.deltaTime);
-    }
-}
