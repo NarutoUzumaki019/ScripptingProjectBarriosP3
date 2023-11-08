dd04b01dc82bf6be91d6ede17a1c38faae06dede
NarutoUzumaki019 <145381119+NarutoUzumaki019@users.noreply.github.com>
2023-11-08 11:45:51 -0800

    I did stuff
    I did stuff

---
+++ ScriptingProjectP3Barrios/Assets/Scripts/AxisRawExample.cs
@@ -0,0 +1,15 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class AxisRawExample : MonoBehaviour;
+{ 
+    public float range;
+    public Text textOutput;
+
+
+   void Update()
+{
+    float
+}
