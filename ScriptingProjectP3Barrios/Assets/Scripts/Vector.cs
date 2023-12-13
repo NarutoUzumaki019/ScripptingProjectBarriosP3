5d7abe7386e4c57363fe0a49f9966fa7b5aaddd2
NarutoUzumaki019 <145381119+NarutoUzumaki019@users.noreply.github.com>
2023-12-13 12:15:26 -0800

    I did stuff
    I did stuff

---
+++ ScriptingProjectP3Barrios/Assets/Scripts/Vector.cs
@@ -0,0 +1,14 @@
+// detects if other transform is behind this object
+
+using UnityEngine;
+using System.Collections;
+
+public class Vector : MonoBehaviour
+{
+    public Transform other;
+
+    private void Update()
+    {
+        if (other)
+    }
+}
